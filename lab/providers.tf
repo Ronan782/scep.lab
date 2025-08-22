0e793ee1204aa232462123d276b56bb80e0344c7
MIM <mim@gmail.com>
2025-08-22 11:46:12 +0200

    changement providers

--- lab/providers.tf
+++ lab/providers.tf
@@ -1,5 +1,13 @@
 terraform {
-  required_version = ">= 1.6.0"
-  required_providers { aws = { source = "hashicorp/aws", version = "~> 5.0" } }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.10.0"
+    }
+  }
 }
-provider "aws" { region = var.aws_region }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
